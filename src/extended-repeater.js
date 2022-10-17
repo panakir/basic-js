aea91fbc920f18efef3d06b389ee2143380a50eb
ponokir <ponokir@gmail.com>
2022-10-17 15:51:14 +0300

    feat: add solution extended-repeater, get-email-domain tasks

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,11 +15,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let res
+  if(options.separator === undefined) {
+    options.separator = "+";
+  }
+  if(options.additionSeparator === undefined) {
+    options.additionSeparator = '|'
+  }
+  if(options.repeatTimes === undefined) {
+    options.repeatTimes = 1;
+  }
+  if(options.additionRepeatTimes === undefined) {
+    options.additionRepeatTimes = 1;
+  }
+  if(options.addition === null) {
+    options.addition = "null";
+  }
+  
+  let addArr = []
+  for(let i = 0; i < options.additionRepeatTimes; i++) {
+    addArr.push(options.addition)
+  }
+  let additionStr = addArr.join(options.additionSeparator)
+  
+  let unit = str + additionStr
+  let unitArr = []
+  for(let i = 0; i < options.repeatTimes; i++) {
+    unitArr.push(unit)
+  }
+  res = unitArr.join(options.separator)
+  return res
 }
-
+  
 module.exports = {
   repeater
 };
